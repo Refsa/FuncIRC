48791eee7f5ea28d4c6ab8de4b525ddaf953da4c
Refsa <refsa22@gmail.com>
2020-01-17 22:23:07 +0100

    added build cmd file for FuncIRC_CLI, updated pipeline file to use this shell file instead of running the project

--- build_funcirc_cli.cmd
+++ build_funcirc_cli.cmd
@@ -5,3 +5,8 @@ echo ####################################
 echo ##### Building FuncIRC Library #####
 echo ####################################
 dotnet build FuncIRC/
+
+echo ################################
+echo ##### Building FuncIRC_CLI #####
+echo ################################
+dotnet build FuncIRC_CLI/
