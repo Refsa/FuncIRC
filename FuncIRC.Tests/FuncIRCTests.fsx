f1d32ecf52d842512bea38fb8b9d347889321d4c
Refsa <refsa22@gmail.com>
2020-01-20 22:05:27 +0100

    FuncIRC: Set up tests project for FuncIRC library

---
+++ FuncIRC.Tests/FuncIRCTests.fsx
@@ -0,0 +1,11 @@
+#r "../FuncIRC/bin/Debug/netstandard2.0/FuncIRC.dll"
+
+namespace FuncIRC.Tests
+
+open NUnit.Framework
+open FuncIRC
+
+module FuncIRCTests =
+    [<Test>]
+    let Test1 () =
+        Assert.Pass()
