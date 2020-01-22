ac0029fbd4ab553e3b469828808c511efc604bd2
Refsa <refsa22@gmail.com>
2020-01-22 21:17:32 +0100

    irc/refactor/ (Cleaned up formatting in MessageParser) #3

--- FuncIRC/IRC/MessageParser.fsx
+++ FuncIRC/IRC/MessageParser.fsx
@@ -12,8 +12,8 @@ module MessageParser =
     open RegexHelpers
     open StringHelpers
 
-    let tagsRegex = @"^(@\S+)"
-    let sourceRegex = @"^(:[\S.]+)"
+    let tagsRegex    = @"^(@\S+)"
+    let sourceRegex  = @"^(:[\S.]+)"
     let commandRegex = @"^([a-zA-Z0-9]+.+)"
 
     /// Trims the tags string of extranous characters and splits it with ';' character
@@ -43,8 +43,7 @@ module MessageParser =
             Some 
                 [
                     for tag in tagSplit -> 
-                        tag.Split('=') 
-                        |> Array.where (fun x -> x <> "")
+                        arrayRemove (tag.Split('=')) stringIsEmpty
                         |> fun kvp -> 
                             match kvp.Length with
                             | IsTagKeyOnly  -> {Key = kvp.[0];   Value = None}
@@ -60,21 +59,21 @@ module MessageParser =
            
         match () with
         | _ when nickSingle && hostSingle && hasPunct -> IsHost
-        | _ when nickSingle && hostSingle -> IsNick
-        | _ when nickSingle && not hostSingle -> IsNickHost
-        | _ when hostSingle && not nickSingle -> IsNickUser
-        | _ when not hostSingle && not nickSingle -> IsNickUserHost
-        | _ -> IsUserHost
+        | _ when nickSingle && hostSingle             -> IsNick
+        | _ when nickSingle && not hostSingle         -> IsNickHost
+        | _ when hostSingle && not nickSingle         -> IsNickUser
+        | _ when not hostSingle && not nickSingle     -> IsNickUserHost
+        | _                                           -> IsUserHost
 
     /// Takes the source extracted in messageSplit and creates a Source record from it
     /// TODO: Look into using FParsec
     let parseSource (sourceString: string option): Source option =
         if sourceString = None then None
         else
-        let sourceString = sourceString.Value
 
-        let nickSplit = arrayRemove (sourceString.Split('!')) stringIsEmpty
-        let hostSplit = arrayRemove (sourceString.Split('@')) stringIsEmpty
+        let sourceString = sourceString.Value
+        let nickSplit    = arrayRemove (sourceString.Split('!')) stringIsEmpty
+        let hostSplit    = arrayRemove (sourceString.Split('@')) stringIsEmpty
 
         match (nickSplit, hostSplit, (sourceString.IndexOf('.') <> -1)) with
         | IsHost         -> Some {Nick = None;               User = None;                           Host = Some sourceString}
@@ -93,14 +92,15 @@ module MessageParser =
     /// Checks the length of the split parameters string to find out if it has trailing parameters or not
     let (|NoParams|HasTrailing|NoTrailing|) (paramSplit: string array) =
         match paramSplit.Length with
-        | 0                  -> NoParams
+        | 0                           -> NoParams
         | 1 when paramSplit.[0] = " " -> NoTrailing
-        | 1                  -> NoTrailing
-        | _                  -> HasTrailing
+        | 1                           -> NoTrailing
+        | _                           -> HasTrailing
 
     /// Picks out the params when there were no trailing marker
     let getParamsNoTrailing (paramsSplit: string array) =
         let subSplit = arrayRemove (paramsSplit.[0].TrimStart(' ').Split(' ')) stringIsEmpty
+
         match subSplit.Length with
         | 1 | 0 -> Some (Parameters [ Parameter paramsSplit.[0] ])
         | _ -> // More than one param found
@@ -109,27 +109,26 @@ module MessageParser =
 
     /// Picks out the parameters when a trailing marker was found
     let getParamsWithTrailing (paramsSplit: string array, parametersString: string) = 
-        let primary = arrayRemove (paramsSplit.[0].Split(' ')) stringIsEmpty |> Array.toList
+        let primary   = arrayRemove (paramsSplit.[0].Split(' ')) stringIsEmpty |> Array.toList
         let secondary = parametersString.Replace(paramsSplit.[0], "") |> fun x -> stringTrimFirstIf (x, ':')
 
         match secondary with
         | "" -> Some (toParameters primary)
-        | _ -> Some (toParameters (primary @ [secondary]))
+        | _  -> Some (toParameters (primary @ [secondary]))
 
     /// Parses the parameters of a message string from just the parameters part
     /// TODO: Look into using FParsec
     let parseParameters (parametersString: string option): Parameters option =
         if parametersString.IsNone then None
         else
+        
         let parametersString = parametersString.Value
-
-        let paramsSplit = arrayRemove (parametersString.Split(':')) stringIsEmpty
+        let paramsSplit      = arrayRemove (parametersString.Split(':')) stringIsEmpty
 
         match paramsSplit with // Check how many trailing params it has
-        | NoParams   -> None
-        | NoTrailing -> getParamsNoTrailing paramsSplit
+        | NoParams    -> None
+        | NoTrailing  -> getParamsNoTrailing paramsSplit
         | HasTrailing -> getParamsWithTrailing (paramsSplit, parametersString)
-            
 
     /// Uses regex to find the different groups of the IRC string message
     /// TODO: Look into using FParsec
@@ -138,37 +137,44 @@ module MessageParser =
         |> fun rest -> // Extract Tags
             let tags = matchRegexFirst rest tagsRegex
             ( // Return Value
-                match tags with
-                | Some tags -> rest.Replace(tags, "").TrimStart(' ')
-                | None -> rest
-                , tags
+                (
+                    match tags with
+                    | Some tags -> rest.Replace(tags, "").TrimStart(' ')
+                    | None      -> rest
+                ), 
+                tags
             )
         |> fun (rest, tags) -> // Extract Source
             let source = matchRegexFirst rest sourceRegex
             ( // Return value
-                match source with
-                | Some source -> rest.Replace(source, "").TrimStart(' ')
-                | None -> rest
-                , tags, source
+                (
+                    match source with
+                    | Some source -> rest.Replace(source, "").TrimStart(' ')
+                    | None        -> rest
+                ), 
+                tags, 
+                source
             )
         |> fun (rest, tags, source) -> // Extract Command (Verb and Params)
             let command = extractString((matchRegexFirst rest commandRegex), extractCommand)
             match command with
-            | None -> (tags, source, None, None)
+            | None         -> (tags, source, None, None)
             | Some command -> 
                 let verb = command.Split(' ').[0].TrimStart(' ')
                 ( // Return Value
                     tags,
                     source,
                     Some verb,
-                    match verb with
-                    | "" -> None
-                    | _ -> Some (command.Replace(verb, "").TrimStart(' '))
+                    (
+                        match verb with
+                        | "" -> None
+                        | _  -> Some (command.Replace(verb, "").TrimStart(' '))
+                    )
                 )
         |> fun (tags, source, verb, parameters) -> // Digest all parts of the message
             { // Construct Message Record
-                Tags = parseTags (extractList (tags, extractTags));
+                Tags   = parseTags   (extractList   (tags, extractTags));
                 Source = parseSource (extractString (source, extractSource));
-                Verb = toVerb verb;
+                Verb   = toVerb verb;
                 Params = parseParameters parameters
             }
